74629a2dc53ce71439c1ab6faeb7e4b03080cce8
Raicuparta <raicuparta@gmail.com>
2019-03-09 15:10:25 +0000

    Updates

---
+++ precache-manifest.387fe1855865e112381d6fd9c81b1ef9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "908ee8ac390cfae3c88e",
+    "url": "/react-redux-life/static/js/runtime~main.0d9a3bb8.js"
+  },
+  {
+    "revision": "762d371f1fb55ded6578",
+    "url": "/react-redux-life/static/js/main.e75f2d36.chunk.js"
+  },
+  {
+    "revision": "084e8d5f321029b509f5",
+    "url": "/react-redux-life/static/js/2.b8f07146.chunk.js"
+  },
+  {
+    "revision": "762d371f1fb55ded6578",
+    "url": "/react-redux-life/static/css/main.1cc812f7.chunk.css"
+  },
+  {
+    "revision": "99a15ae9587d35d23e74eec025b54058",
+    "url": "/react-redux-life/index.html"
+  }
+];
